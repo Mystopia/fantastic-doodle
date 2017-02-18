8beeb7a31b9b8bc4bf3e21e3ebe038f10c5d33ff
Ryan Balfanz <rbalfanz@gmail.com>
2017-02-18 11:05:20 -0800

    Add blooming-cliffs-50597.herokuapp.com to ALLOWED_HOSTS

--- service/settings/production.py
+++ service/settings/production.py
@@ -4,6 +4,10 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = False
 
+ALLOWED_HOSTS = [
+    'blooming-cliffs-50597.herokuapp.com',
+]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
