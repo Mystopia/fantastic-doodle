43e7572ec0468f44a3d6ede126e20f1a8cfed318
Ryan Balfanz <rbalfanz@gmail.com>
2017-03-20 23:43:51 -0700

    Remove hardcoded ALLOWED_HOSTS

--- service/settings/production.py
+++ service/settings/production.py
@@ -4,10 +4,6 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    'fantastic-doodle--production.herokuapp.com'
-]
-
 ALLOWED_HOSTS = [h.strip() for h in os.getenv('ALLOWED_HOSTS', '').split(',') if h]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
