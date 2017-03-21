5f4db52983be9580cbf6c194575583df6a31a88f
Ryan Balfanz <rbalfanz@gmail.com>
2017-03-20 23:41:21 -0700

    Add ALLOWED_HOSTS environment variable

--- service/settings/production.py
+++ service/settings/production.py
@@ -8,6 +8,8 @@ ALLOWED_HOSTS = [
     'fantastic-doodle--production.herokuapp.com'
 ]
 
+ALLOWED_HOSTS = [h.strip() for h in os.getenv('ALLOWED_HOSTS', '').split(',') if h]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
