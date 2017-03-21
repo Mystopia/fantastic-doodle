d1e5fc0513ce4861712d8a8bda677a7e6c0290e2
Ryan Balfanz <rbalfanz@gmail.com>
2017-03-21 00:25:20 -0700

    SSL/HTTPS
    https://docs.djangoproject.com/en/1.10/topics/security/#ssl-https

--- service/settings/production.py
+++ service/settings/production.py
@@ -9,3 +9,17 @@ ALLOWED_HOSTS = [h.strip() for h in os.getenv('ALLOWED_HOSTS', '').split(',') if
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# SSL/HTTPS Security
+
+## Set SECURE_SSL_REDIRECT to True, so that requests over HTTP are redirected to HTTPS.
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+
+## Use ‘secure’ cookies.
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+## Use HTTP Strict Transport Security (HSTS)
+SECURE_HSTS_SECONDS = 3600
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
