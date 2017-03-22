7e98b63fe76254a67a008e60ebd0f1682fa77611
Ryan Balfanz <rbalfanz@gmail.com>
2017-03-21 23:25:48 -0700

    Enable WhiteNoise by wrapping WSGI application
    - https://devcenter.heroku.com/articles/django-assets#whitenoise
    - http://whitenoise.evans.io/en/stable/base.html

--- service/settings/base.py
+++ service/settings/base.py
@@ -14,19 +14,4 @@ INSTALLED_APPS += [
     'django_extensions',
 ]
 
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-
-    # Enable WhiteNoise
-    # http://whitenoise.evans.io/en/stable/django.html#enable-whitenoise
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
 DATABASES['default'] = dj_database_url.config()
--- service/wsgi.py
+++ service/wsgi.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "service.settings")
 
 application = get_wsgi_application()
+# https://devcenter.heroku.com/articles/django-assets#whitenoise
+application = DjangoWhiteNoise(application)
